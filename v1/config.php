c94c1ed56846de46cdf404c5a82e5785adf79468
KsandraLekva <117514193+KsandraLekva@users.noreply.github.com>
2023-02-05 00:33:17 +0500

    Add files via upload

---
+++ v1/config.php
@@ -0,0 +1,10 @@
+<?php
+define("DB" , array(
+    "login" => "u0941340_lai",
+    "password" => "wJ6qL3cK3esN0bA2",
+    "host" => "localhost",
+    "name" => "u0941340_lai",
+    "charset" => "utf8mb4"
+));
+
+  define('TIME_ZONE', 'Asia/Tashkent');  
